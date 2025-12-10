0257b8abf6aae1332185a45da54ced43224da426
arata-e <47027066+arata-e@users.noreply.github.com>
2025-12-10 14:40:14 +0300

    Updated nitro-nuxt.d.ts

--- .nuxt/types/nitro-nuxt.d.ts
+++ .nuxt/types/nitro-nuxt.d.ts
@@ -1,5 +1,8 @@
 
-
+/// <reference path="app.config.d.ts" />
+/// <reference path="runtime-config.d.ts" />
+/// <reference types="/home/project/node_modules/@nuxt/nitro-server/dist/index.mjs" />
+/// <reference path="nitro-middleware.d.ts" />
 
 import type { RuntimeConfig } from 'nuxt/schema'
 import type { H3Event } from 'h3'
