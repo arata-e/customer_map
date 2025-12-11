c7a3d10732fd015484230ea498ca24506e927979
arata-e <47027066+arata-e@users.noreply.github.com>
2025-12-11 11:53:57 +0300

    Updated amap.vue

--- pages/amap.vue
+++ pages/amap.vue
@@ -27,7 +27,9 @@ import { ref, onMounted, onUnmounted } from 'vue'
 import AddressSearch from '~/components/AddressSearch.vue'
 import ObjectSearch from '~/components/ObjectSearch.vue'
 import ResetViewButton from '~/components/ResetViewButton.vue'
-import Result, StatusClose from '@bitrix24/b24jssdk'
+import Result from '@bitrix24/b24jssdk'
+import StatusClose from '@bitrix24/b24jssdk'
+  
 const mapElement = ref(null)
 const mapReady = ref(false)
 const loadingMessage = ref('Инициализация...')
